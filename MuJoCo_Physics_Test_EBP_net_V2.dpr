ce11473b4a8ed4a5ef2f343db784ad94d10d0767
Roelof Emmerink <rpe86@hotmail.com>
2025-11-11 19:01:35 +0100

    Update MuJoCo_Physics_Test_EBP_net_V2.dpr

--- MuJoCo_Physics_Test_EBP_net_V2.dpr
+++ MuJoCo_Physics_Test_EBP_net_V2.dpr
@@ -1,6 +1,6 @@
 {**************************************************************************
   Project : MuJoCo EBP Demo (Delphi/FMX)
-  File    : MuJoCo_Physics_Test_EBP_net_V2.pas
+  File    : MuJoCo_Physics_Test_EBP_net_V2.dpr
   Author  : Roelof Emmerink
   Year    : 2025
   License : MIT
