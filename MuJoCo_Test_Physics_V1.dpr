cebfe528e0460b3dcd74aab2a5029c68d9a087dd
Roelof Emmerink <rpe86@hotmail.com>
2025-11-11 19:00:51 +0100

    Update MuJoCo_Test_Physics_V1.dpr

--- MuJoCo_Test_Physics_V1.dpr
+++ MuJoCo_Test_Physics_V1.dpr
@@ -1,3 +1,33 @@
+{**************************************************************************
+  Project : MuJoCo Test Physics (Delphi/FMX)
+  File    : MuJoCo_Test_Physics_V1.dpr
+  Author  : Roelof Emmerink
+  Year    : 2025
+  License : MIT
+  SPDX-License-Identifier: MIT
+
+  ------------------------------------------------------------------------
+  Copyright (c) 2025 Roelof Emmerink
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+***************************************************************************}
+
 program MuJoCo_Test_Physics_V1;
 
 uses
@@ -10,7 +40,7 @@ uses
 begin
   Application.Initialize;
 
-  // Maak een resource-loze form (géén .fmx nodig)
+  // Maak een resource-loze form (gÃ©Ã©n .fmx nodig)
   Form1 := TForm1.CreateNew(nil);
 
   // Init alle logica (eigenschappen/venster + MuJoCo-start)
